0193cafbb45ab8c468da09c36bc05456d5ded376
Ale Vasquez <jose5m45@gmail.com>
2021-10-29 10:58:32 -0600

    Created posts retrieve, post card obj, and redirects.

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,60 @@
-export default function HomePage() {
+import fs from "fs";
+import path from "path";
+import matter from "gray-matter";
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import Post from "@/components/Post";
+
+
+export default function HomePage({posts}) {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Layout>
+      <h1 className="text-5xl border-b-4 p-5 font-bold">
+        Latest Posts
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5">
+        {
+          posts.map((post, index)=>{
+            return (
+              <Post key={index} post={post} />
+            )
+          })
+        }
+      </div>
+      <Link href='/blog'>
+        <a
+          className="
+            block text-center border border-gray-500 text-gray-800
+            rounded-md py-4 my-5 transition duration-500 ease select-node
+            hover:text-white hover:bg-gray-900
+            focus:outline-none focus:shadow-outline w-full
+          "
+        >
+          All Posts
+        </a>
+      </Link>
+    </Layout>
   )
+}
+
+export async function getStaticProps({}) {
+
+  const files = fs.readdirSync(path.join('posts'))
+
+  const posts = files.map((postFileName)=> {
+    let slug = postFileName.replace('.md', '');
+    let markdownWithMeta = fs.readFileSync(path.join('posts', postFileName), 'utf-8')
+    const {data: frontmatter} = matter(markdownWithMeta);
+    return {
+      slug,
+      frontmatter
+    }
+  })
+
+  return {
+    props: {
+      posts
+    }
+  }
 }
